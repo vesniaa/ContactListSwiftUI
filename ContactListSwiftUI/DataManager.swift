b3eb30d1aacffcedea82b852b5ec0076d3b3841d
Eugenia <pyssiki@yandex.ru>
2022-05-26 20:52:10 +0500

    data manager

--- ContactListSwiftUI/DataManager.swift
+++ ContactListSwiftUI/DataManager.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    private init() {}
+    
+    let names = [
+    "Bruce", "Sharon",
+    "Carl", "Allan",
+    "Nicola", "Steven",
+    "Ted", "Aaron",
+    "Tim", "Jack"
+    ]
+    
+    let surnames = [
+    "Black", "Smith",
+    "Butler", "Dow",
+    "Pennyworth", "Williams",
+    "Jankin", "Robertson",
+    "Murphy", "Jennies"
+    ]
+    
+    let phoneNumbers = [
+        "756295649", "501285927",
+        "509296346", "567392702",
+        "0129748411", "1122324455",
+        "1234667890", "0987674321",
+        "6543007193", "0192837465"
+    ]
+    
+    let emails = [
+        "bruce@ya.ru", "sharon@gmail.com",
+        "carl@mir.ru", "allan@boss.org",
+        "nicola@yard.ru", "steven@parent.ru",
+        "ted@apple.com", "aaron@usa.com",
+        "tim@ru.ru", "jack@world.org"
+    ]
+    
+}
