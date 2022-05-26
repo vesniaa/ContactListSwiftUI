cdadff55e991eb43e4ee731316d5ef6455488e65
Eugenia <pyssiki@yandex.ru>
2022-05-26 17:42:12 +0500

    datamanager and person

---
+++ ContactListSwiftUI/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  ContactListSwiftUI
+//
+//  Created by Евгения Аникина on 26.05.2022.
+//
+
+import Foundation
---
+++ ContactListSwiftUI/PersonView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonView.swift
+//  ContactListSwiftUI
+//
+//  Created by Евгения Аникина on 26.05.2022.
+//
+
+import SwiftUI
+
+struct PersonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonView()
+    }
+}
